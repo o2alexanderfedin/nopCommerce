0b8c77726469e1cc84be5b97590150e053cfde0b
o2alex <af@O2.services>
2025-01-08 14:39:21 -0800

    Fix Demo plugin: Use INopFileProvider for path mapping

--- src/Plugins/Nop.Plugin.Demo.BasicPlugin/Controllers/LicenseController.cs
+++ src/Plugins/Nop.Plugin.Demo.BasicPlugin/Controllers/LicenseController.cs
@@ -2,17 +2,25 @@ using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Nop.Core;
+using Nop.Core.Infrastructure;
 using Nop.Web.Framework.Controllers;
 
 namespace Nop.Plugin.Demo.BasicPlugin.Controllers
 {
     public class LicenseController : BasePluginController
     {
+        private readonly INopFileProvider _fileProvider;
+
+        public LicenseController(INopFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
         [HttpGet]
         [Route("/vendor-location/license")]
         public async Task<IActionResult> Index()
         {
-            var licenseFilePath = Path.Combine(CommonHelper.MapPath("~/Plugins/Demo.BasicPlugin"), "LICENSE.md");
+            var licenseFilePath = _fileProvider.MapPath("~/Plugins/Demo.BasicPlugin/LICENSE.md");
             if (!System.IO.File.Exists(licenseFilePath))
                 return NotFound();
 
